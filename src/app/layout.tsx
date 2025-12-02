29c43ccfcb45fb4e1455344101ff0c7c9c611ecf
maicol1912 <maicolarcila34@gmail.com>
2025-12-01 22:47:50 -0500

    Use larger favicon images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,12 +24,15 @@ export const metadata: Metadata = {
   creator: "Maicol Arcila",
   icons: {
     icon: [
-      { url: `${basePath}/favicon-16x16.png`, sizes: '16x16', type: 'image/png' },
+      { url: `${basePath}/android-chrome-512x512.png`, sizes: '512x512', type: 'image/png' },
+      { url: `${basePath}/android-chrome-192x192.png`, sizes: '192x192', type: 'image/png' },
       { url: `${basePath}/favicon-32x32.png`, sizes: '32x32', type: 'image/png' },
+      { url: `${basePath}/favicon-16x16.png`, sizes: '16x16', type: 'image/png' },
     ],
     apple: [
       { url: `${basePath}/apple-touch-icon.png`, sizes: '180x180', type: 'image/png' },
     ],
+    shortcut: `${basePath}/android-chrome-512x512.png`,
   },
   manifest: `${basePath}/site.webmanifest`,
   openGraph: {
